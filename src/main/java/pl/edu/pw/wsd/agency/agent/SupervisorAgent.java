6a1352d34c96c6fb492e92e7e8df87e95a48ce1e
Pawel <kurowski.pawel1@gmail.com>
2016-01-08 16:00:46 +0100

    stworzenie wstępnego nadzorcy

---
+++ src/main/java/pl/edu/pw/wsd/agency/agent/SupervisorAgent.java
@@ -0,0 +1,53 @@
+package pl.edu.pw.wsd.agency.agent;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
+public class SupervisorAgent extends BaseAgent {
+
+	private static final long serialVersionUID = 8604486033349286351L;
+
+	private static final Logger log = LogManager.getLogger();
+	
+	@Override
+    protected void setup() {
+        super.setup();
+        registerAgent();
+        
+        // TODO: add behaviours
+    }
+
+    @Override
+    protected void takeDown() {
+        try {
+            DFService.deregister(this);
+        } catch (FIPAException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Registers Agent in DF Agent's YellowPages.
+     */
+    private void registerAgent() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("supervisor");
+        sd.setName(getLocalName());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException e) {
+            e.printStackTrace();
+            log.error("Could not register agent. Agent terminating");
+            doDelete();
+        }
+    }
+}
--- src/main/java/pl/edu/pw/wsd/agency/container/SupervisorContainer.java
+++ src/main/java/pl/edu/pw/wsd/agency/container/SupervisorContainer.java
@@ -1,15 +1,18 @@
 package pl.edu.pw.wsd.agency.container;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import pl.edu.pw.wsd.agency.agent.SupervisorAgent;
 import pl.edu.pw.wsd.agency.agent.meta.JadeAgentDescription;
 
 public class SupervisorContainer extends BaseContainer {
 
 	@Override
 	public List<JadeAgentDescription> getAgentsToRun() {
-		// TODO Auto-generated method stub
-		return null;
+		List<JadeAgentDescription> descriptions = new ArrayList<>();
+		descriptions.add(createAgentDescription(SupervisorAgent.class, "SupervisorAgent.properties"));
+		return descriptions;
 	}
 
 }
