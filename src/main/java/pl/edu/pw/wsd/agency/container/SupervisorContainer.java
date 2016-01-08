8db7129eade0c81f60958ed00d31e05e5b88a711
mczerwi <marcinczrw@gmail.com>
2016-01-08 14:56:12 +0100

    Pusty kontener nadzorcy

---
+++ src/main/java/pl/edu/pw/wsd/agency/container/SupervisorContainer.java
@@ -0,0 +1,15 @@
+package pl.edu.pw.wsd.agency.container;
+
+import java.util.List;
+
+import pl.edu.pw.wsd.agency.agent.meta.JadeAgentDescription;
+
+public class SupervisorContainer extends BaseContainer {
+
+	@Override
+	public List<JadeAgentDescription> getAgentsToRun() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
